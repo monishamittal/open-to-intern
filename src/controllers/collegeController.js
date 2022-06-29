d964ad18e7983c49a20af5daf49e33b570ab73f0
Mohd-Farhan-9990 <mohdfarhan9639@gmail.com>
2022-06-29 12:29:42 +0530

    Updated

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,18 +1,34 @@
 const collegeModel = require("../models/collegeModel")
+const internModel = require("../models/internModel")
 
 
 const createColleges = async function(req,res){
-    let data = req.body
-    const dataCreated = await collegeModel.create(data)
-    res.status(201).send({status:true, data:dataCreated})
+      try{
+            let data = req.body
+            const dataCreated = await collegeModel.create(data)
+            res.status(201).send({status:true, data:dataCreated})
+      }
+      catch(err){
+        return res.status(500).send({status:false,})
+      }
 }
 
 const collegeDetails = async function(req,res){
-    let college = req.query
-    const result = await collegeModel.find({college})
-    console.log(result)
-    res.send(result)
-
+    try{
+            
+            let collegeName = req.query
+            const college = await collegeModel.findOne(collegeName)
+            // console.log(college)
+            const [collegeDetails] = college
+            
+            const id = collegeDetails._id.toString()
+            const interns = await internModel.find({collegeId:id})
+              Object.assign(college,{interns:"interns"})
+            return res.status(200).send({status:true,msg:"List Of The Interns Of This College",data:college})
+    }
+    catch(err){
+        return res.status(500).send({status:false,msg:err.message})
+    }
 }
 
 
