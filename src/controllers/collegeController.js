8fb0237931aecb2b5820b7db03952f3e16bfc3f7
Mohd-Farhan-9990 <mohdfarhan9639@gmail.com>
2022-06-29 12:59:05 +0530

    Updated

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -17,14 +17,17 @@ const collegeDetails = async function(req,res){
     try{
             
             let collegeName = req.query
-            const college = await collegeModel.findOne(collegeName)
-            // console.log(college)
-            const [collegeDetails] = college
-            
-            const id = collegeDetails._id.toString()
+            let college = await collegeModel.findOne(collegeName)
+            const{_id,name,fullName,logoLink} = college
+            const id = _id.toString()
             const interns = await internModel.find({collegeId:id})
-              Object.assign(college,{interns:"interns"})
-            return res.status(200).send({status:true,msg:"List Of The Interns Of This College",data:college})
+            college.interns =interns
+            // college.assign = {
+            //     interns:interns
+            // }
+            //   Object.assign(college,{interns:"interns"})
+            // college.interns ="interns"
+            return res.status(200).send({status:true,msg:"List Of The Interns Of This College",data:{name,fullName,logoLink,interns}})
     }
     catch(err){
         return res.status(500).send({status:false,msg:err.message})
